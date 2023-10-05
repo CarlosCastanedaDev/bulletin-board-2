87270630d0318a314bafb06a8627852c3f212626
CarlosCastanedaDev <carlos@nemin.dev>
2023-10-05 13:44:20 -0500

    update sample data task

--- app/models/board.rb
+++ app/models/board.rb
@@ -6,7 +6,10 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Board < ApplicationRecord
   validates(:name, presence: true, uniqueness: true)
+  # belongs_to :user, class_name: "user", foreign_key: "user_id"
+  # has_many :posts, class_name: "post", foreign_key: "user_id"
 end
